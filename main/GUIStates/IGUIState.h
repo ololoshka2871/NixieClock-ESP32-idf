267dcfa10611634cf5b9e83f45ee96aae34b5f43
shiloxyz <shiloxyz@q6600arch>
2019-09-24 19:54:54 +0300

    Transitions

--- main/GUIStates/IGUIState.h
+++ main/GUIStates/IGUIState.h
@@ -1,6 +1,12 @@
 #ifndef _IGUI_STATE_H_
 #define _IGUI_STATE_H_
 
-struct IGUIState {};
+#include "IGUIStateTransition.h"
+
+struct IGUIState {
+  IGUIStateTransition *clickTransition;
+  IGUIStateTransition *LongPushTransition;
+  IGUIStateTransition *IdleTransition;
+};
 
 #endif /* _IGUI_STATE_H_ */
---
+++ main/GUIStates/IGUIStateTransition.h
@@ -0,0 +1,10 @@
+#ifndef _IGUI_STATE_TRANSITION_
+#define _IGUI_STATE_TRANSITION_
+
+#include "IGUIState.h"
+
+struct IGUIStateTransition {
+  IGUIState *targetState;
+};
+
+#endif /* _IGUI_STATE_TRANSITION_ */
